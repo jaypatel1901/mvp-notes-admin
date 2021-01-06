2db7c96e4343933e873003402baaafe690147007
jaypatel1901 <j.patel@braininventory.com>
2021-01-07 01:45:06 +0530

    npm change

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonService } from '../../core/services/common.service';
 import { NgxPaginationModule } from 'ngx-pagination';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgxSpinnerService } from "ngx-spinner";
 import SlimSelect from 'slim-select'
 
 
@@ -21,7 +22,15 @@ export class UsersComponent implements OnInit {
   isDetails = true
   Registered:string
   planList:any=[]
-  constructor(private commonService: CommonService) {
+  invitationFormError = {
+    "firstName": '',
+    "lastName":'',
+    "email": '',
+    "subscriptionsId": ''
+  }
+  invitationForm: FormGroup
+
+  constructor(private commonService: CommonService,private fb: FormBuilder,private spinner: NgxSpinnerService) {
 
   }
 
@@ -50,6 +59,74 @@ export class UsersComponent implements OnInit {
       }
     })
   }
+// 
+inviationvalidationMessages = {
+  'firstName': {
+    'required': 'First Name is required',
+    'minlength': 'minimum 3 characters required'
+  },
+  'lastName': {
+    'required': 'Last Name is required',
+    'minlength': 'minimum 3 characters required'
+  },
+  'email': {
+    'required': 'email is required',
+    'pattern': 'email not in valid format'
+  },
+  'subscriptionId': {
+    'required': 'subscription Plan is required',
+  }
+}
+createInvitation() {
+
+  this.invitationForm = this.fb.group({
+    firstName: ['', [Validators.required, Validators.minLength(3)]],
+    lastName: ['', [Validators.required, Validators.minLength(3)]],
+    email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')]],
+    subscriptionId: ['', [Validators.required]]
+  })
+  this.invitationForm.valueChanges.subscribe(data => this.onValueChanges(data))
+}
+onValueChanges(data?: any) {
+  if (!this.invitationForm)
+    return
+  const form = this.invitationForm;
+  for (const field in this.invitationFormError) {
+    if (this.invitationFormError.hasOwnProperty(field)) {
+      this.invitationFormError[field] = '';
+      const control = form.get(field);
+      if (control && control.dirty && !control.valid) {
+        const messages = this.inviationvalidationMessages[field];
+        for (const key in control.errors) {
+          if (control.errors.hasOwnProperty(key)) {
+            this.invitationFormError[field] += messages[key] + ' ';
+          }
+        }
+      }
+    }
+  }
+}
+onSendInvitation() {
+  this.spinner.show();
+  let userData = {
+    firstName: this.invitationForm.value.firstName,
+    lastName: this.invitationForm.value.lastName,
+    email: this.invitationForm.value.email,
+    subscriptionId: this.invitationForm.value.subscriptionId
+  }
+  console.log("onSendInvitation", userData)
+  this.commonService.post('invatationUser',userData).subscribe((data: any) => {
+    if (data.status == 200) {
+      this.spinner.hide();
+      alert("Invitation sent successfully!")
+    } else {
+      this.spinner.hide();
+      alert(data.message)
+    }
+  })
+}
+// 
+
   getSubscriptionPlanList() {
     this.commonService.get('getSubscriptionPlan').subscribe((data: any) => {
       if (data.status == 200) {
